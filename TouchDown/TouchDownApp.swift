317485ffcbbe05eed0187c9fdf40a1de9fb41630
Sun Fang <sunlitamo@gmail.com>
2021-08-22 16:00:35 -0400

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Fang Sun on 2021-08-22.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
